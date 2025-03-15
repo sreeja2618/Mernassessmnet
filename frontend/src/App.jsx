095b20c90776d7f5d2f90e7094c66a4e335534e4
sreeja2618 <2211cs010571@mallareddyuniversity.ac.in>
2025-03-15 17:26:13 +0530

    Fixed duplicate Router issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ApplyLeave from "./components/ApplyLeave";
 import CheckBalance from "./components/CheckBalance";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/apply-leave" element={<ApplyLeave />} />
-        <Route path="/check-balance" element={<CheckBalance />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/apply-leave" element={<ApplyLeave />} />
+      <Route path="/check-balance" element={<CheckBalance />} />
+    </Routes>
   );
 }
 
